e8a7002ca7cbb0d515c0afaacb5654c1810c8906
Eden Cohen <cohen10@kenyon.edu>
2025-10-16 14:24:08 -0400

    submit

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -8,7 +8,52 @@ LinkedList::~LinkedList() {
 }
 void LinkedList::push_front(int v) { head_ = new Node(v, head_); }
 void LinkedList::remove_evens() {
-    // TODO: implement
+    if (head_ == nullptr) return; //no elements in list
+
+    if (head_->next == nullptr) { //1 element in list
+        if (head_->value %2 == 1) {
+            return;
+        } else {
+            delete head_;
+            head_ = nullptr;
+            return;
+        }
+    }
+
+    //we know there are at least 2 elemetns
+
+    Node* newNode = head_->next; // #2
+
+    while (newNode->value %2 == 0) {
+        if (newNode->next == nullptr) {
+            delete head_->next;
+            head_->next = nullptr;
+            return;
+        } else {
+            newNode = newNode->next;
+            delete head_->next;
+            head_->next = newNode;
+        }
+    }
+
+
+    while (newNode->next != nullptr) {
+
+        if (newNode->next->value %2 == 0) {
+            
+            if (newNode->next->next == nullptr) {
+                delete newNode->next;
+                newNode->next = nullptr;
+                return;
+            } else {
+                Node* nextNode = newNode->next->next;
+                delete newNode->next;
+                newNode->next = nextNode;
+            }
+        }
+        newNode = newNode->next;
+    }
+    
 }
 std::string LinkedList::to_string() const {
     std::ostringstream oss;
